64a81409c79b829f5c8f060b948edbbab53a5dcf
JamesCHamilton <165865052+JamesCHamilton@users.noreply.github.com>
2025-01-25 15:07:55 -0500

    please work :)

--- backend/websocket/Websocket.js
+++ backend/websocket/Websocket.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { verifyToken } from "../jwtMiddleware.js"
+import { io } from "socket.io-client";
 import { socket } from "server/router";
 import PtoDChat from "../schemas/PtoDChat.js";
 
@@ -50,3 +50,31 @@ const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("Express.js with WebSocket!");
+  });
+  
+
+
+
+
+
+  const socket = new WebSocket("ws://localhost:5000");
+
+  socket.onopen = () => {
+    console.log("WebSocket connection established.");
+    socket.send("Hello, Server!");
+  };
+  
+  socket.onmessage = (event) => {
+    console.log("Message from server:", event.data);
+  };
+  
+  socket.onclose = () => {
+    console.log("WebSocket connection closed.");
+  };
+  
+  socket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+  };
