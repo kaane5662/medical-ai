e86134426a7cf57b76ded4f0bf7ad04bc66247dd
JamesCHamilton <165865052+JamesCHamilton@users.noreply.github.com>
2025-01-25 14:45:22 -0500

    PtoDChat schema

---
+++ backend/schemas/PtoDChat.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const patientToDoctorSchema = new mongoose.Schema({
+    roomID:{
+        type:String,
+        required:true,
+    },
+
+    senderRole:{
+        type:String,
+        required:true,
+    },
+
+    message:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+
+    timeStamp:{
+        type:
+            Date, default: Date.now,
+            required:true
+    },
+
+    patient:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Patient"
+    },
+
+    
+    timestamp: { type: Date, default: Date.now }, 
+})
